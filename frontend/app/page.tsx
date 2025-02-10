993ac0673e512d07b63d0c6c76f95d89ebb9e4a6
Dominic Camill <106774701+LikeMyGames@users.noreply.github.com>
2025-02-10 12:53:46 -0800

    updated page.tsx file

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,49 +12,9 @@ import { useState } from "react";
 
 
 export default function Home() {
-
-	const [user, setUser] = useState({
-		username: "babyyoda",
-		password: "pizzaburger",
-		lists: [
-			{
-				id: "this is my new id",
-				name: "data",
-				tasks: [],
-			}
-		],
-		settings: {
-			ListDirectory: "./",
-			APIPath: "localhost:8080/",
-			DeleteCompletedTasks: false,
-		},
-	})
-
-	const [list, setList] = useState({
-		id: "this is my new id",
-		name: "data",
-		tasks: [],
-	})
-
-	const OpenList = (): void => {
-		fetch(user.settings.APIPath + "?/")
-	}
-
 	return (
-		<div className={style["main"]}>
-			<ListsMenu lists={user.lists} menuItemOnClick={OpenList}/>
-			<ListView list={list} />
-			<EditPanel></EditPanel>
-        	
-        	
-        	<div className={style["edit_panel"]}>
-        	    <h2 className={style["properties"]}>
-					Properties
-        	    </h2>
-				<div className={style["edit_panel_replaceable"]}>
-
-				</div>
-        	</div>
-    	</div>
+		<>
+			
+		</>
 	);
 }
