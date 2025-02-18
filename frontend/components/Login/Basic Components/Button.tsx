b2311f4686ca2933c1dbdabd0cf21ee02731618d
Dominic Camill <camilldominic@gmail.com>
2025-02-18 14:31:38 -0800

    working on local use ui

--- frontend/components/Login/Basic Components/Button.tsx
+++ frontend/components/Login/Basic Components/Button.tsx
@@ -1,9 +1,10 @@
 import style from "@/components/Login/Login.module.css"
+import { MouseEventHandler } from "react";
 
-export const TextInput: React.FC<{ children: React.ReactNode; className?: string; type?: "submit" | "reset" | "button"; }> = ({ children, className, type }) => {
+export const TextInput: React.FC<{ children: React.ReactNode; className?: string; type?: "submit" | "reset" | "button"; onClick?: MouseEventHandler<HTMLButtonElement>; }> = ({ children, className, type, onClick }) => {
     return (
-        <div className={style.card_input}>
-            <button className={`${style.card_button} ${className}`} type={type}>{children}</button>
+        <div className={ style.card_input }>
+            <button className={ `${ style.card_button } ${ className }` } type={ type } onClick={ onClick }>{ children }</button>
         </div>
     )
 }
--- frontend/components/Login/LoginOption.tsx
+++ frontend/components/Login/LoginOption.tsx
@@ -8,53 +8,134 @@ import Button from "@/components/Login/Basic Components/Button"
 import GoogleIcon from "@/public/google.svg"
 import GithubIcon from "@/public/github.svg"
 import Image from "next/image";
-import Head from "next/head";
-
+import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function LoginOption() {
-	return (
-		<div className={style.main}>
-			<Head>
-				<link rel="icon" type="image/png" href="@/public/icons/favicon/favicon-96x96.png" sizes="96x96" />
-				<link rel="icon" type="image/svg+xml" href="@/public/icons/favicon/favicon.svg" />
-				<link rel="shortcut icon" href="@/public/icons/favicon/favicon.ico" />
-			</Head>
-			<button type={"button"} className={`${style.card} ${style.card_small}`}>
-				<Icon>person_add</Icon>
-			</button>
-			<form action={login} className={`${style.card} ${style.card_big}`}>
-				<Icon>person</Icon>
-				<h2 className={style.card_text_main}>Sign In</h2>
-				<TextInput type="email" title={"email"}>Username or Email</TextInput>
-				<TextInput type="password" title={"password"}>Password</TextInput>
-				<Button className="special" type={"submit"}>
-					<h3 className={style.card_button_text}>Sign In</h3>
-				</Button>
-				<div className={style.card_option_seperator}>
-					<div className={style.card_option_seperator_bar} />
-					<h4 className={style.card_option_seperator_text}>or continue with</h4>
-					<div className={style.card_option_seperator_bar} />
-				</div>
-				<button type={"button"} className={style.card_option}>
-					<Image alt="Google Icon" src={GoogleIcon} className={style.card_option_icon}></Image>
-					<h3 className={style.card_button_text} style={{
-						color: "var(--text-color)"
-					}}>
-						Google
-					</h3>
+	const [choice, setChoice] = useState("login");
+
+	if(choice === "login") {
+		return (
+			<div className={style.main}>
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("create")}}>
+					<Icon>person_add</Icon>
 				</button>
-				<button type={"button"} className={style.card_option}>
-					<Image alt="Github Icon" src={GithubIcon} className={style.card_option_icon}></Image>
-					<h3 className={style.card_button_text} style={{
-						color: "var(--text-color)"
-					}}>
-						Github
-					</h3>
+				<form action={login} className={`${style.card} ${style.card_big}`}>
+					<Icon>person</Icon>
+					<h2 className={style.card_text_main}>Sign In</h2>
+					<TextInput type="email" title={"email"}>Username or Email</TextInput>
+					<TextInput type="password" title={"password"}>Password</TextInput>
+					<Button className="special" type={"submit"}>
+						<h3 className={style.card_button_text}>Sign In</h3>
+					</Button>
+					<div className={style.card_option_seperator}>
+						<div className={style.card_option_seperator_bar} />
+						<h4 className={style.card_option_seperator_text}>or continue with</h4>
+						<div className={style.card_option_seperator_bar} />
+					</div>
+					<button type={"button"} className={style.card_option}>
+						<Image alt="Google Icon" src={GoogleIcon} className={style.card_option_icon}></Image>
+						<h3 className={style.card_button_text} style={{
+							color: "var(--text-color)"
+						}}>
+							Google
+						</h3>
+					</button>
+					<button type={"button"} className={style.card_option}>
+						<Image alt="Github Icon" src={GithubIcon} className={style.card_option_icon}></Image>
+						<h3 className={style.card_button_text} style={{
+							color: "var(--text-color)"
+						}}>
+							Github
+						</h3>
+					</button>
+				</form>
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("local")}}>
+					<Icon>devices</Icon>
 				</button>
-			</form>
-			<button type={"button"} className={`${style.card} ${style.card_small}`}>
-				<Icon>devices</Icon>
-			</button>
-		</div>
-	);
+			</div>
+		);
+	} else if (choice  === "create") {
+		return (
+			<div className={style.main}>
+				{/* <Head>
+					<link rel="icon" type="image/png" href="@/public/icons/favicon/favicon-96x96.png" sizes="96x96" />
+					<link rel="icon" type="image/svg+xml" href="@/public/icons/favicon/favicon.svg" />
+					<link rel="shortcut icon" href="@/public/icons/favicon/favicon.ico" />
+				</Head> */}
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("local")}}>
+					<Icon>devices</Icon>
+				</button>
+				<form action={login} className={`${style.card} ${style.card_big}`}>
+					<Icon>person_add</Icon>
+					<h2 className={style.card_text_main}>Create Account</h2>
+					<TextInput type="email" title={"email"}>Username or Email</TextInput>
+					<TextInput type="password" title={"password"}>Password</TextInput>
+					<Button className="special" type={"submit"}>
+						<h3 className={style.card_button_text}>Create Account</h3>
+					</Button>
+					<div className={style.card_option_seperator}>
+						<div className={style.card_option_seperator_bar} />
+						<h4 className={style.card_option_seperator_text}>or continue with</h4>
+						<div className={style.card_option_seperator_bar} />
+					</div>
+					<button type={"button"} className={style.card_option}>
+						<Image alt="Google Icon" src={GoogleIcon} className={style.card_option_icon}></Image>
+						<h3 className={style.card_button_text} style={{
+							color: "var(--text-color)"
+						}}>
+							Google
+						</h3>
+					</button>
+					<button type={"button"} className={style.card_option}>
+						<Image alt="Github Icon" src={GithubIcon} className={style.card_option_icon}></Image>
+						<h3 className={style.card_button_text} style={{
+							color: "var(--text-color)"
+						}}>
+							Github
+						</h3>
+					</button>
+				</form>
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("login")}}>
+					<Icon>person</Icon>
+				</button>
+			</div>
+		);
+	} else if (choice === "local") {
+		return (
+			<div className={style.main}>
+				{/* <Head>
+					<link rel="icon" type="image/png" href="@/public/icons/favicon/favicon-96x96.png" sizes="96x96" />
+					<link rel="icon" type="image/svg+xml" href="@/public/icons/favicon/favicon.svg" />
+					<link rel="shortcut icon" href="@/public/icons/favicon/favicon.ico" />
+				</Head> */}
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("login")}}>
+					<Icon>person</Icon>
+				</button>
+				<form action={login} className={`${style.card} ${style.card_big}`}>
+					<Icon>devices</Icon>
+					<h2 className={style.card_text_main}>Use Locally</h2>
+					<Button className="special" type={"button"}>
+						<h3 className={style.card_button_text}>On Browser</h3>
+					</Button>
+					<p className={style.card_button_descriptor}>Stores data in your browser</p>
+					<Button className="special" type={"button"}>
+						<h3 className={style.card_button_text}>On Device</h3>
+					</Button>
+					<p className={style.card_button_descriptor}>Stores data in directly on your device</p>
+					<div className={style.card_option_seperator}>
+						<div className={style.card_option_seperator_bar} />
+						<h4 className={style.card_option_seperator_text}>or continue with</h4>
+						<div className={style.card_option_seperator_bar} />
+					</div>
+					<Button className="special" onClick={() => redirect("/docs")} >
+						<h3 className={style.card_button_text}>Learn More</h3>
+					</Button>
+				</form>
+				<button type={"button"} className={`${style.card} ${style.card_small}`} onClick={() => {setChoice("create")}}>
+					<Icon>person_add</Icon>
+				</button>
+			</div>
+		)
+	}
 }
