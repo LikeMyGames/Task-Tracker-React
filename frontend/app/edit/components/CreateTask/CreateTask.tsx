f3eb24a8dc61adac867f61584d456ca6436d9d5a
Dominic Camill <106774701+LikeMyGames@users.noreply.github.com>
2025-03-31 22:06:38 -0700

    need to continue working on create task interface

--- frontend/app/edit/components/CreateTask/CreateTask.tsx
+++ frontend/app/edit/components/CreateTask/CreateTask.tsx
@@ -1,4 +1,4 @@
-import { List } from "@/components/List";
+// import { List } from "@/components/List";
 import style from "./CreateTask.module.css"
 import { Task } from "@/components/Task";
 import { useState } from "react";
@@ -8,15 +8,15 @@ interface Options {
     batch: boolean;
 }
 
-export function CreateTask({ list, close }: { list: List; close: () => void }) {
-    // const [newTasks, setNewTasks] = useState<Task[]>([])
+export function CreateTask() {
+    const [newTasks, setNewTasks] = useState<Task[]>([])
     const [options, setOptions] = useState<Options>({} as Options)
     return (
         <div className={style.create}>
             <div className={style.create_panel}>
-                <h1 className={style.create_panel_title}>Create Task or Tasks</h1>
-                <div className={`${style.create_subpanel} ${style.h}`}>
-                    <button type="button" className={`${style.create_subpanel_button} ${!options.batch ? style.create_subpanel_button_selected : ""}`} onClick={() => {
+                <h1 className={style.create_panel_title}>{options.batch ? "Create Tasks" : "Create Task"}</h1>
+                <div className={`${style.create_create_type} ${style.h}`}>
+                    <button type="button" className={`${style.create_create_type_button} ${!options.batch ? style.create_create_type_button_selected : ""}`} onClick={() => {
                         setOptions({
                             ...options,
                             batch: false
@@ -26,7 +26,7 @@ export function CreateTask({ list, close }: { list: List; close: () => void }) {
                             Single
                         </h4>
                     </button>
-                    <button type="button" className={`${style.create_subpanel_button} ${options.batch ? style.create_subpanel_button_selected : ""}`} onClick={() => {
+                    <button type="button" className={`${style.create_create_type_button} ${options.batch ? style.create_create_type_button_selected : ""}`} onClick={() => {
                         setOptions({
                             ...options,
                             batch: true
@@ -37,28 +37,51 @@ export function CreateTask({ list, close }: { list: List; close: () => void }) {
                         </h4>
                     </button>
                 </div>
-                <div className={`${style.create_subpanel} ${style.v}`}>
-                    <div className={`${style.create_subpanel_item} ${style.h}`} >
-                        <Icon className={style.create_subpanel_item_icon} iconName={"chevron_right"} />
-                        <input className={style.create_subpanel_subitem}>
+                <div className={`${style.create_task_list}`}>
+                    <div className={`${style.create_task_list_item}`} >
+                        <Icon className={style.create_task_list_item_icon} iconName={"chevron_right"} />
+                        {/* <div className={style.create_task_list_item_input_container}> */}
+                        <h3 className={style.create_task_list_item_input}>Name</h3>
+                        {/* <select title={"task severity selector"} className={style.create_task_list_item_input} defaultValue={3}>
+                            <option value={1}>Very Low</option>
+                            <option value={2}>Low</option>
+                            <option value={3}>Medium</option>
+                            <option value={4}>High</option>
+                            <option value={5}>Very High</option>
+                        </select>
+                        <input className={style.create_task_list_item_input} placeholder="Due Date" type="date">
 
                         </input>
-                        <input className={style.create_subpanel_subitem}>
+                        <input className={style.create_task_list_item_input} placeholder="Due Time" type="time">
 
                         </input>
-                        <input className={style.create_subpanel_subitem}>
+                        <input className={style.create_task_list_item_input} placeholder="Notes" type="text"> */}
 
-                        </input>
-                        <input className={style.create_subpanel_subitem}>
-
-                        </input>
+                        {/* </input> */}
+                        {/* </div> */}
                     </div>
-                    <div className={`${style.create_subpanel_item} ${style.h}`} >
-                        <Icon className={style.create_subpanel_item_icon} iconName={"chevron_right"} />
-                        <input className={style.create_subpanel_subitem}>
-
-                        </input>
-                    </div>
+                </div>
+                <div className={`${style.create_actions}`}>
+                    <button type="button" id="add" className={`${style.create_action_button} ${style.flex_start}`} onClick={() => {
+                        setOptions({
+                            ...options,
+                            batch: false
+                        })
+                    }}>
+                        <h4>
+                            Add Task
+                        </h4>
+                    </button>
+                    <button type="button" id="create" className={`${style.create_action_button} ${style.flex_end}`} onClick={() => {
+                        setOptions({
+                            ...options,
+                            batch: true
+                        })
+                    }}>
+                        <h4>
+                            {options.batch ? "Create Tasks" : "Create Task"}
+                        </h4>
+                    </button>
                 </div>
             </div>
         </div >
